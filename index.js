87b86c2ec3d0977bd0916bec80a8a3dc98f22f54
T3zz <110574162+T3zz@users.noreply.github.com>
2022-08-17 09:03:15 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+const yesBtn = document.querySelector('#yesBtn');
+
+yesBtn.addEventListener('click',function() { 
+    alert('Gracias por la respuesta ;)')
+});
+
+const noBtn = document.querySelector('#noBtn');
+
+
+ noBtn.addEventListener('click',function() { 
+        alert('???creo que te equivocaste de boton. :)')
+    });
